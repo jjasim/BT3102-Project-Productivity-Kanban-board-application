250107f93e6fed62c94fe0a05534d3ccb0d07e01
Haris Dzulkifli <haris@dbasri.com>
2023-03-14 00:16:22 +0800

    project setup with router and blank login page

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <LoginPage/>
+</template>
+
+<script>
+import LoginPage from './views/LoginPage.vue';
+
+export default {
+  name: 'App',
+  components:{
+    LoginPage
+  },
+  data(){
+    return {
+      refreshComp: 0
+    }
+  },
+  methods: {
+    change(){
+      this.refreshComp += 1
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
